5bd8831e09c54e675f7a730a6dd4959393e7027f
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-01-07 20:54:43 +0530

    Update merge_sort.cpp

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -2,11 +2,14 @@ void merge(int input[],int s,int e,int m)
 {
     int si=m-s+1,ei=e-m;
     int l[si],r[ei];
+	
     for(int i=0;i<si;i++)
         l[i]=input[s+i];
      for(int i=0;i<ei;i++)
         r[i]=input[m+i+1];
+	
     int k=s,j=0,i1=0;
+ //compare and enter the lowest
     while(j<si && i1<ei)
     {
         if(l[j]<=r[i1])
@@ -14,7 +17,7 @@ void merge(int input[],int s,int e,int m)
         else
             input[k]=r[i1],k++,i1++;
     }
-    
+    //left over parts of l and r
     while(j<si)
         input[k]=l[j],k++,j++;
     while(i1<ei)
@@ -24,13 +27,15 @@ void merge(int input[],int s,int e,int m)
 void merges(int input[],int s,int e)
 {  int mid =(s+e)/2;
     if(s>=e) return ;
+ //splitting
      merges(input,s,mid);
       merges(input,mid+1,e);
+ //merging
      merge(input,s,e,mid);
     
 }
 void mergeSort(int input[], int size){
-	// Write your code here
+	
      merges(input,0,size-1);
      
 }
