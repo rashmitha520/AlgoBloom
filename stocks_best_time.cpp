8cae8d1794aa9e878e365d1706bc2033d65c3bdb
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-01-08 22:53:42 +0530

    Create stocks_best_time.cpp

---
+++ stocks_best_time.cpp
@@ -0,0 +1,20 @@
+/*Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.*/
+
+#include<bits/stdc++.h>
+#include<climits>
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+      int i=0,j=prices.size();int maxi=0;int mini=INT_MAX;       
+        while(i<j){
+        mini=min(mini,prices[i]);
+        maxi=max(maxi,prices[i]-mini);
+            i++;
+        }
+        return maxi;
+    }
+};
