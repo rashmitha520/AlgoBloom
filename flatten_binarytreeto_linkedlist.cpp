403a2788ec8e06dee1d0bf92bff9383ead966841
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-01-15 11:10:15 +0530

    Update flatten_binarytreeto_linkedlist.cpp

--- flatten_binarytreeto_linkedlist.cpp
+++ flatten_binarytreeto_linkedlist.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     void flatten(TreeNode* root) {
      if(!root) return;
+        //first put left nodes on right;
+        // then add right nodes later;
       TreeNode* r=root->right,*n=root;
      root->right=root->left;
         root->left=nullptr;
