bede34faa4d5749d5b40e079adba50a0a41f50ad
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-01-15 17:14:48 +0530

    Create zigzaglevelordertraversal.cpp
    Also called spiral traversal

---
+++ zigzaglevelordertraversal.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        int level=0;
+        if(root==NULL) return {};
+        queue<TreeNode*> q;
+        q.push(root);
+          vector<vector<int>> res;
+        while(!q.empty())
+        {
+            int n=q.size();
+            vector<int> v(n);
+           for(int i=0;i<n;i++)
+           {
+               TreeNode* t=q.front();
+               q.pop();
+               if(level==0)
+               v[i]=t->val;
+               else
+                   v[n-i-1]=t->val;
+               
+               if(t->left) q.push(t->left);
+               if(t->right) q.push(t->right);
+           }
+            res.push_back(v);
+            level=!level;
+        }
+        return res;
+    }
+};
