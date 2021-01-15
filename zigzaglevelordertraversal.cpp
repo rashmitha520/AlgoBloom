018d6307e651ccd9a656b71510f7e2986667dda7
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-01-15 17:16:32 +0530

    Update zigzaglevelordertraversal.cpp

--- zigzaglevelordertraversal.cpp
+++ zigzaglevelordertraversal.cpp
@@ -10,6 +10,7 @@ public:
         {
             int n=q.size();
             vector<int> v(n);
+           //in for loop for each nodes
            for(int i=0;i<n;i++)
            {
                TreeNode* t=q.front();
@@ -17,12 +18,15 @@ public:
                if(level==0)
                v[i]=t->val;
                else
+                   //reverse pushback when odd
                    v[n-i-1]=t->val;
                
                if(t->left) q.push(t->left);
                if(t->right) q.push(t->right);
            }
             res.push_back(v);
+            //if level==0 then !level=1 and vice versa
+            //for even it becomes 0 and odd its 1.
             level=!level;
         }
         return res;
