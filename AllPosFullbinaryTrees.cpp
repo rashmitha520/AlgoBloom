50b73d444606de60e0aecff9c49ceb088925b48a
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-01-21 21:23:59 +0530

    Update AllPosFullbinaryTrees.cpp

--- AllPosFullbinaryTrees.cpp
+++ AllPosFullbinaryTrees.cpp
@@ -1,4 +1,21 @@
-??
+/*
+eg: 7
+when k=1 , then l=func(1),r=func(7-1-1)
+when k=2 ,then l=func(2) as n=2 and even numbers dont form full binary trees ,the recursive function returns empty array.
+when k=3, l=func(3),r=func(7-3-1)
+when k=4 same as k=2
+when k=5 then l=func(5) r=func(1)
+k=1---->
+ 
+ 0                       
+/ \
+0  0
+  / \
+ 0   0     
+/\
+0 0  left or right depending on recursion
+
+*/
 class Solution {
 public:
     vector<TreeNode*> allPossibleFBT(int N) {
