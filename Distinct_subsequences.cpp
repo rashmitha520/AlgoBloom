e50864ae230694849a7762237f5c367ded85b942
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-01-26 23:48:44 +0530

    Update Distinct_subsequences.cpp

--- Distinct_subsequences.cpp
+++ Distinct_subsequences.cpp
@@ -23,3 +23,13 @@ int numDistinct(string s, string t) {
         }
         return dp[m][n];
     }
+//better approach
+    int numDistinct(string s, string t) {
+        int m = t.length(), n = s.length();
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        for (int j = 0; j <= n; j++) dp[0][j] = 1;
+        for (int j = 1; j <= n; j++)
+            for (int i = 1; i <= m; i++)
+                dp[i][j] = dp[i][j - 1] + (t[i - 1] == s[j - 1] ? dp[i - 1][j - 1] : 0);
+        return dp[m][n];
+    }
