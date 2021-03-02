2c53aa81a1c925212fe5b58961b0f228571ff383
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-03-02 16:23:10 +0530

    Create pattern matching.cpp

---
+++ Arrays/string/pattern matching.cpp
@@ -0,0 +1,26 @@
+
+//brute force: O(N*M) complexity
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+  string s,t;
+  cin>>s>>t;
+  int m=s.length(),n=t.length();
+  int k;
+  for(int i=0;i<m-n;i++)
+  { if(s[i]!=t[k]) continue;
+   bool isp=1;
+   k=i;
+    for(int j=0;j<n;j++)
+    {
+      if(s[j+i]!=t[j]) {
+        isp=0;
+        break;
+        };
+      
+    }
+    if(isp) cout<<k+1;
+    else cout<<0;
+  }
+}
