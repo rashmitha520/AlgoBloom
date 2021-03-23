39da9539e0bc55a3b53ea2a6b05adc3e594f2da0
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-03-23 14:30:34 +0530

    Update Candies.cpp

--- dp/Candies.cpp
+++ dp/Candies.cpp
@@ -22,14 +22,21 @@ class Solution {
 public:
     int candy(vector<int>& r) {
         int n=r.size();
-     vector<int> d(n,1);   
+        vector<int> d(n,1);
+      
         for(int i=1;i<n;i++)
-            if(r[i-1]<r[i])
-                d[i]=d[i-1]+1;
+        { 
+          if(r[i-1]<r[i])
+           d[i]=d[i-1]+1;
+        }
+      
         int sum=d[n-1];
+      
         for(int i=n-2;i>=0;i--)
-        { if(r[i]>r[i+1])
-                d[i]=max(d[i],d[i+1]+1);
+        { 
+          if(r[i]>r[i+1])
+            d[i]=max(d[i],d[i+1]+1);
+          
          sum+=d[i];
         }
         return sum;
