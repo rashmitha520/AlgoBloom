494c21b1011348c759785d084b0c7f1f1751b8ec
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-04-08 14:20:13 +0530

    Update Subarrays with K Different Integers.cpp

--- misc/Subarrays with K Different Integers.cpp
+++ misc/Subarrays with K Different Integers.cpp
@@ -19,3 +19,50 @@ public:
 };
 
 //sliding window
+class Solution {
+public:
+      int atmostk(vector<int>& arr, int k) {
+        int i = -1, j = -1, ans = 0;
+        unordered_map<int, int> m;
+        const int n = arr.size();
+         while(true)
+         { //until valid
+             while(i<(n-1))
+             {
+               i++;
+               m[arr[i]]++;
+               if(m.size()<=k)
+               {
+                   ans+=(i-j);
+               }
+               else break;
+             }
+             
+             if(i==n-1 && m.size()<=k)
+                 break;
+             
+             //to make window valid
+             //i.e. map size <=k 
+             //then count no of such subarrays
+             while(j<i)
+             {
+              j++;
+              if(m[arr[j]]==1)
+                  m.erase(arr[j]);
+              else
+                  m[arr[j]]--;
+              if(m.size()>k) continue;
+              else
+              {
+                  ans+=(i-j);
+                  break;
+              }
+             }
+             
+         }
+       return ans;
+    }
+    int subarraysWithKDistinct(vector<int>& A, int K) {
+       return atmostk(A,K)-atmostk(A,K-1); 
+    }
+};
