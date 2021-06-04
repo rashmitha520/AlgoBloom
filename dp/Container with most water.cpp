4fc9ae276d42df594088e83ab4655b56b77421d5
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-06-04 12:39:17 +0530

    Update Container with most water.cpp

--- dp/Container with most water.cpp
+++ dp/Container with most water.cpp
@@ -12,3 +12,18 @@ int maxArea(vector<int>& height) {
     
         return area;
     }
+//2-ptr approach
+ int maxArea(vector<int>& height) {
+     int area=0;
+        int i=0,j=height.size()-1;
+        while(i<j)
+        {
+           area=max(area,(j-i)*min(height[i],height[j]));
+           
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        return area;
+    }
