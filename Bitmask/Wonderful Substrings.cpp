c25b3a60c85a694a6dc6aa845923c69246a8b6f5
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-07-01 20:36:06 +0530

    Create Wonderful Substrings.cpp

---
+++ Bitmask/Wonderful Substrings.cpp
@@ -0,0 +1,16 @@
+long long wonderfulSubstrings(string word) {
+      long mask=0,res=0;
+        map<long long int,long long int> count;
+        count[0]=1;
+        for(auto c:word)
+        {
+            mask^=(1<<(c-'a'));
+            res+=count[mask]; //for even num
+            for(int i=0;i<10;i++)
+            {
+                res+=count[mask^(1<<i)];//for odd num
+            }
+            count[mask]++;
+        }
+        return res;
+    }
