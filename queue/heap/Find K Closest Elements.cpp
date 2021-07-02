7dd23be2f24eaa55e49481a54e1852fb8718c40e
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2021-07-02 16:49:16 +0530

    Create Find K Closest Elements.cpp

---
+++ queue/heap/Find K Closest Elements.cpp
@@ -0,0 +1,16 @@
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        vector<int> ans;
+      
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> p;
+        for(int i:arr)
+        {
+            p.push({abs(i-x),i});
+        }
+        for(int i=0;i<k;i++)
+        {   
+            ans.push_back(p.top().second);
+            p.pop();
+        }
+        sort(begin(ans),end(ans));
+        return ans;
+    }
