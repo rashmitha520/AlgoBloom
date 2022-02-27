d52e0b459f57f1ebc1e3e53f018f69a8734be8a5
Rashmitha <57208322+rashmitha520@users.noreply.github.com>
2022-02-27 11:27:43 +0530

    Update Robot Bounded In Circle.cpp

--- google int/Robot Bounded In Circle.cpp
+++ google int/Robot Bounded In Circle.cpp
@@ -1,3 +1,22 @@
+//another approach
+class Solution {
+public:
+    bool isRobotBounded(string ins) {
+       vector<pair<int,int>> dir ={{0,1},{1,0},{0,-1},{-1,0}};
+       int x=0,y=0,z=0;
+        for(int i=0;i<ins.size();i++){
+           if(ins[i]=='L') z=(z+3)%4;
+           else if(ins[i]=='R') z=(z+1)%4;
+           else{
+               x+=dir[z].first;
+               y+=dir[z].second;
+           }
+        }
+        return(( x==0 && y==0) || z!=0);
+    }
+};
+
+//2nd approach
 class Solution {
 public:
     bool isRobotBounded(string instructions) {
